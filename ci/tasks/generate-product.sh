b2b59b57ef9135d9a7cc527b9eb1c5a4a5e60c5f
Dr Nic Williams <drnicwilliams@gmail.com>
2017-01-15 21:34:46 -0800

    initial attempt

---
+++ ci/tasks/generate-product.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e # fail fast
+set -x # print commands
+
+mkdir -p tile/tmp/metadata
+mkdir -p workspace/metadata
+mkdir -p workspace/releases
+mkdir -p workspace/content_migrations # opsmgr v1.6
+cp -r tile/migrations workspace/ # opsmgr v1.7
+
+TILE_VERSION=$(cat tile-version/number)
+
+cat pivnet-stemcell/metadata.json
+STEMCELL_VERSION=$(cat pivnet-stemcell/metadata.json | jq -r ".Release.Version")
+
+cat >tile/tmp/metadata/version.yml <<EOF
+---
+product_version: "${TILE_VERSION}"
+stemcell_criteria:
+  version: "${STEMCELL_VERSION}"
+EOF
+
+cat >tile/tmp/metadata/releases.yml <<YAML
+---
+releases:
+YAML
+
+# versions available via inputs
+boshreleases=("vault" "broker-registrar")
+for boshrelease in "${boshreleases[@]}"
+do
+  release_version=$(cat ${boshrelease}/version)
+  cat >>tile/tmp/metadata/releases.yml <<YAML
+  - name: ${boshrelease}
+    file: ${boshrelease}-${release_version}.tgz
+    version: "${release_version}"
+YAML
+  if [[ -f ${boshrelease}/release.tgz ]]; then
+    cp ${boshrelease}/release.tgz workspace/releases/${boshrelease}-${release_version}.tgz
+  fi
+  if [[ -f ${boshrelease}/${boshrelease}-${release_version}.tgz ]]; then
+    cp ${boshrelease}/${boshrelease}-${release_version}.tgz workspace/releases/
+  fi
+done
+
+spruce merge --prune meta \
+  tile/templates/metadata/base.yml \
+  tile/templates/metadata/stemcell_criteria.yml \
+  tile/tmp/metadata/version.yml \
+  tile/tmp/metadata/releases.yml \
+  tile/templates/metadata/form_types.yml \
+  tile/templates/metadata/property_blueprints.yml \
+  tile/templates/metadata/job_storage.yml \
+    > workspace/metadata/dingo-backup-storage.yml
+
+
+sed -i "s/RELEASE_VERSION_MARKER/${TILE_VERSION}/" workspace/metadata/dingo-backup-storage.yml
+
+cat workspace/metadata/dingo-backup-storage.yml
+
+cd workspace
+ls -laR .
+
+echo "creating dingo-backup-storage-${TILE_VERSION}.pivotal file"
+zip -r dingo-backup-storage-${TILE_VERSION}.pivotal content_migrations migrations metadata releases
+
+mv dingo-backup-storage-${TILE_VERSION}.pivotal ../product
+ls ../product
---
+++ ci/tasks/re-version-product.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e # fail fast
+set -x # show commands
+
+next_tile_version=$(cat tile-version/number)
+
+tile_path=$(pwd)/$(ls generated-tile/dingo-backup-storage*.pivotal)
+next_tile_path=$(pwd)/reversioned-product/dingo-backup-storage-${next_tile_version}.pivotal
+
+zip_tile_path=$(pwd)/generated-tile/dingo-backup-storage.zip
+mv ${tile_path} ${zip_tile_path}
+unzip -u ${zip_tile_path} -d unpack
+TILE_VERSION=$(cat unpack/metadata/dingo-backup-storage.yml| yaml2json | jq -r .product_version)
+echo "Previous version $TILE_VERSION; re-versioning to $next_tile_version"
+
+echo Updating metadata/dingo-backup-storage.yml
+sed -i -e "s/^product_version:.*$/product_version: \"${next_tile_version}\"/" unpack/metadata/dingo-backup-storage.yml
+cat unpack/metadata/dingo-backup-storage.yml
+
+echo Looking up all previous versions to generate content_migrations/dingo-backup-storage.yml
+./tile/ci/tasks/opsmgr16_content_migration.rb ${next_tile_version} unpack/content_migrations/dingo-backup-storage.yml
+cat unpack/content_migrations/dingo-backup-storage.yml
+
+cd unpack
+zip -r -f ${zip_tile_path} *
+
+unzip -l ${zip_tile_path}
+
+mv ${zip_tile_path} ${next_tile_path}
+
+ls -al ${next_tile_path}
